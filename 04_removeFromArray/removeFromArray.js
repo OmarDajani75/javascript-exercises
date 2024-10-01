04a25d3b585383a77abd77891e91e15eab461aae
Omar Dajani <omardajani079@gmail.com>
2024-10-01 12:27:42 -0700

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+  newArray = [];
+  arr.forEach((item) => {
+	if (item !== nums);
+	newArray.push(item)
+      });
+      return newArray;
 };
 
 // Do not edit below this line
