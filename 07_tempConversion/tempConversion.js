d036b2974297b50d30d90a3af4b406f9549b4620
Omar Dajani <omardajani079@gmail.com>
2024-10-09 12:16:06 -0700

    Finish 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  cel = (faren - 32) * 5/9;
+  cel_round = Math.round(cel * 10) / 10;
+  return cel_round;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  faren = (cel * 9/5) + 32;
+  faren_round = Math.round(faren * 10) / 10;
+  return faren_round;
 };
 
 // Do not edit below this line
@@ -9,3 +15,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// Math.round(num * 10) / 10
